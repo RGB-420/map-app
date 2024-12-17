e63a187753fba584434a2dbbc859d74fc9705bb9
Raúl García <73raul7373@gmail.com>
2024-12-17 17:15:36 +0100

    Update data.py

--- streets_db/data.py
+++ streets_db/data.py
@@ -1,32 +1,14 @@
 import requests
+import osmnx as ox
+
 from streets_db import Streets_db
 
 class Data:
     # Obtain street data from Overpass API
-    def get_data_overpass(city):
-        # Overpass API URL
-        overpass_url = "http://overpass-api.de/api/interpreter"
-
-        # Overpass query to get streets in the given city, excluding certain types
-        overpass_query = f"""
-            [out:json];
-            area[name="{city}"]->.searchArea;
-            (
-             way["highway"]["highway" != "motorway"]["highway" != "trunk"]["highway" != "motorway_link"]["highway" !="cycleway"]["highway" !="service"]["footway" != "crossing"]["access" != "private"](area.searchArea);
-            );
-            out body;
-            >;
-            out skel qt;
-        """
-        
-        # Sending the GET request to the Overpass API
-        response = requests.get(overpass_url, params={'data': overpass_query})
-        
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            return response.json() # Return the data in JSON format
-        else:
-            raise Exception(f"Error obtaining data from the API: {response.status_code}")
+    def get_data(city):
+        graph = ox.graph_from_place(city, network_type="walk")
+        _, Data.edges = ox.graph_to_gdfs(graph)
+                
 
     # Function to get the elevation for a given latitude and longitude using the Open-Elevation API
     def get_elevation(latitude, longitude):
@@ -50,31 +32,15 @@ class Data:
 
     # Process data from API and store it in the db
     def process_data(con, data):
-        # Create a nodes dictionary for ID
-        nodes_dict = {}
+        for _, edge in Data.edges.iterrows():
+            if edge["geometry"] is not None:
+                name = edge.get("name", "Calle Sosa")
 
-        # Store the nodes in the dict
-        for element in data['elements']:
-            if element['type'] == 'node':
-                nodes_dict[element['id']] = (element['lat'], element['lon'])
+                if isinstance(name, list):
+                    name = name[0]
 
-        # Store the streets
-        for element in data['elements']:
-            if element['type'] == 'way' and 'tags' in element and 'name' in element['tags']:
-                street_name = element['tags']['name']
+                geometry = edge["geometry"].wkt
 
-                if element['tags'].get('lanes', None):
-                    car_lanes = element['tags']['lanes']
-                else:
-                    car_lanes = None
-                # Insert the street in the db and take the ID
-                id_street = Streets_db.insert_street(con, street_name, car_lanes)
+                id_street = Streets_db.insert_street(con, name)
 
-                # Insert the street nodes
-                for node_id in element['nodes']:
-                    if node_id in nodes_dict:
-                        latitude, longitude = nodes_dict[node_id]
-
-                        elevation = Data.get_elevation(latitude, longitude)
-
-                        Streets_db.insert_node(con, id_street, latitude, longitude, elevation)
+                Streets_db.insert_node(con, id_street, geometry)
